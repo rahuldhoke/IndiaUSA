a2ea9f901b300c6c559845e695dffa82f847afea
rahul <dhokerahuls@gmail.com>
2022-05-21 17:31:09 +0530

    usa1 commit

---
+++ src/main/java/USA/USA1/App.java
@@ -0,0 +1,13 @@
+package USA.USA1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/USA/USA1/USAClass1.java
@@ -0,0 +1,14 @@
+package USA.USA1;
+
+public class USAClass1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	
+	public void test1() {
+		System.out.println("this is test 1");
+	}
+}
